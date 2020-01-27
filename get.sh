27c34b7d7bbd9bed53456eada512ce95ad9647b4
Martin <martinfz@t-online.de>
2020-01-27 12:56:19 +0100

    Rearranged the repository structure

---
+++ get.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+GIT_URL="https://raw.githubusercontent.com/zwoefler/Dot-File-Repo/master";
+RESET='\033[0m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+
+echo -e "${GREEN}Starting Configuration... ${RESET}"
+
+# Install config files
+echo -e "${YELLOW}Retreiving .bashrc from  "
