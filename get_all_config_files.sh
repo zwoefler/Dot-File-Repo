0f2dde677bbc56f9f69c7237630bddecbd377604
Zwoelfer <martinfz@t-online.de>
2020-06-07 00:19:23 +0200

    Deleteing unnecessary comments

--- get_all_config_files.sh
+++ get_all_config_files.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Define all paths to dotfiles
 declare -A DOTFILES_LOCATION
 
 DOTFILES_LOCATION[vim]="${HOME}/.vimrc"
@@ -10,27 +9,20 @@ DOTFILES_LOCATION[xfce4]="${HOME}/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4
 vs_code_extensions="code --list-extensions >> $(dirname "${BASH_SOURCE}")/vscode/vscode_extensions.txt"
 
 
-# Try receiving Dotfiles path from the list of dotfiles from previous step
 receive_dotfile_from_paths() {
-    # Iterate over dict items
     for key in "${!DOTFILES_LOCATION[@]}"; do
-        # print current item to receive dotfile
         if [[ -e ${DOTFILES_LOCATION[${key}]} ]]; then
             echo "Receiving Dotfile from ${key}"
-            # copy item to dotfile repo folder
             rsync -avh --no-perms \
             "${DOTFILES_LOCATION[${key}]}" \
             "$(dirname "${BASH_SOURCE}")/${key}"
             echo ""
         elif [[ ! -e "${DOTFILES_LOCATION[$key]}" ]]; then
-            # In case of failure, just go on.
             echo "The Config-file for ${DOTFILE_LOCATION[${key}]} could not be found"
             echo ""
         fi
-    # Continue with next item
     done
 }
-# Overwrite Dotfile from path, into corresponding directory
 
 receive_dotfile_from_paths
 unset receive_dotfile_from_paths
